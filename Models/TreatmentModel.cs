df543f42976a9037f459b62f2d30708ba7826200
Valasa-Andrei <andreivalasa7@gmail.com>
2025-03-24 13:50:59 +0200

    this commit includes the fixed TreatmentModel

--- Models/TreatmentModel.cs
+++ Models/TreatmentModel.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Documents;
 using Team3.Entities;
 
 namespace Team3.Models
@@ -11,8 +12,8 @@ namespace Team3.Models
     public class TreatmentModel
     {
         private static TreatmentModel? _instance;
+        private static readonly object _lock = new object();
         private readonly Config _config;
-        private Task<List<Treatment>> _treatments;
 
         private TreatmentModel()
         {
@@ -22,11 +23,15 @@ namespace Team3.Models
         public static TreatmentModel Instance
         {
             get
-            {   if(_instance == null)
+            {
+                lock (_lock)
                 {
-                    _instance = new TreatmentModel();
+                    if (_instance == null)
+                    {
+                        _instance = new TreatmentModel();
+                    }
                 }
-            return _instance;
+                return _instance;
             }
         }
 
