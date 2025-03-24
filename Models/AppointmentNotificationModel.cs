54527394fc76140f26ad857a09b55cac961651b5
donFelippe <filip.rus@yahoo.com>
2025-03-24 17:21:01 +0200

    Models for appointed classes Check if SqlCommands are doing fine, todo: check errors about sqlclient classs

--- Models/AppointmentNotificationModel.cs
+++ Models/AppointmentNotificationModel.cs
@@ -1,12 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Team3.Entities;
 
 namespace Team3.Models
 {
-    class AppointmentNotificationModel
+    public class AppointmentNotificationModel
     {
+        private static AppointmentNotificationModel? _instance;
+        private readonly Config _config;
+
+        private AppointmentNotificationModel()
+        {
+            _config = Config.Instance;
+        }
+
+        public static AppointmentNotificationModel Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new AppointmentNotificationModel();
+                }
+                return _instance;
+            }
+        }
+
+        public void AddAppointmentNotification(AppointmentNotification notification)
+        {
+            const string query = "INSERT INTO AppointmentNotifications (Id, DeliveryDateTime, Message, AppointmentId) VALUES (@Id, @DeliveryDateTime, @Message, @AppointmentId);";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(Config.CONNECTION))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@Id", notification.Id);
+                    command.Parameters.AddWithValue("@DeliveryDateTime", notification.DeliveryDateTime);
+                    command.Parameters.AddWithValue("@Message", notification.Message);
+                    command.Parameters.AddWithValue("@AppointmentId", notification.AppointmentId);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error adding appointment notification", e);
+            }
+        }
+
+        public List<AppointmentNotification> GetUpcomingAppointmentNotifications()
+        {
+            const string query = "SELECT * FROM AppointmentNotifications WHERE DeliveryDateTime >= GETDATE();";
+            List<AppointmentNotification> notifications = new List<AppointmentNotification>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(Config.CONNECTION))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            notifications.Add(new AppointmentNotification(
+                                (int)reader["Id"],
+                                (DateTime)reader["DeliveryDateTime"],
+                                reader["Message"].ToString(),
+                                (int)reader["AppointmentId"]
+                            ));
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error retrieving appointment notifications", e);
+            }
+
+            return notifications;
+        }
     }
 }
--- Models/MedicalRecordModel.cs
+++ Models/MedicalRecordModel.cs
@@ -1,12 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Team3.Entities;
 
 namespace Team3.Models
 {
-    class MedicalRecordModel
+    public class MedicalRecordModel
     {
+        private static MedicalRecordModel? _instance;
+        private readonly Config _config;
+
+        private MedicalRecordModel()
+        {
+            _config = Config.Instance;
+        }
+
+        public static MedicalRecordModel Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new MedicalRecordModel();
+                }
+                return _instance;
+            }
+        }
+
+        public void AddMedicalRecord(MedicalRecord medicalRecord)
+        {
+            const string query = "INSERT INTO MedicalRecords (Id, PatientId) VALUES (@Id, @PatientId);";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(Config.CONNECTION))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@Id", medicalRecord.Id);
+                    command.Parameters.AddWithValue("@PatientId", medicalRecord.PatientId);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error adding medical record", e);
+            }
+        }
+
+        public MedicalRecord GetMedicalRecord(int id)
+        {
+            const string query = "SELECT * FROM MedicalRecords WHERE Id = @Id;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(Config.CONNECTION))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new MedicalRecord((int)reader["Id"], (int)reader["PatientId"]);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error retrieving medical record", e);
+            }
+
+            return null;
+        }
     }
 }
