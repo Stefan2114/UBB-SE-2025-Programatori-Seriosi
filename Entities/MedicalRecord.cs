ee3c068208d06e08cf52d083b5f2146a8083db63
donFelippe <filip.rus@yahoo.com>
2025-03-31 13:11:52 +0300

    Swapped from missing classes mock to actua classes

--- Entities/MedicalRecord.cs
+++ Entities/MedicalRecord.cs
@@ -11,20 +11,13 @@ public class MedicalRecord
     public int Id { get; set; }
     public int PatientId { get; set; }
 
-    public int DoctorId { get; set; }
-
     public List<Treatment> Treatments { get; set; } = new List<Treatment>();
     public List<Review> Reviews { get; set; } = new List<Review>();
 
-    public MedicalRecord(int id, int patientId, int doctorId)
+    public MedicalRecord(int id, int patientId)
     {
         Id = id;
         PatientId = patientId;
-        DoctorId = doctorId;
     }
-
-    public override string ToString()
-    {
-        return $"[MedicalRecord] ID: {Id}, Patient ID: {PatientId}, Doctor ID : {DoctorId}";
-    }
+ 
 }
