a008f4dbab61a01cae70235920f5b700924cdb50
PaulSchiop <schioppaul@gmail.com>
2025-03-31 11:16:04 +0300

    ceva paul
    incerc cevai

--- Config.cs
+++ Config.cs
@@ -9,9 +9,13 @@ namespace Team3
 {
     public class Config
     {
+<<<<<<< HEAD
 
 
         public static readonly string CONNECTION = "Server=vm;Database=team3;Integrated Security=True;";
+=======
+        public static readonly string CONNECTION = "Server=PAUL;Database=team3;Integrated Security=True;";
+>>>>>>> 2679f05 (ceva paul)
         public static readonly TimeZoneInfo ROMANIA_TIMEZONE;
 
         private static Config? _instance;
