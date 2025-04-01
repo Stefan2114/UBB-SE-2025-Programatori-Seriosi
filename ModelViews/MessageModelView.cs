68bafc09092b76e69985c60057671c270957f212
Stefan2114 <stefan.toma021@gmail.com>
2025-04-01 02:49:51 -0700

    fix: message modelview and view problems

--- ModelViews/MessageModelView.cs
+++ ModelViews/MessageModelView.cs
@@ -18,76 +18,42 @@ namespace Team3.ModelViews
     {
         private readonly MessageModel messageModel;
         private readonly UserModelView userModelView;
-        private int chatId;
-        private int userId;
+
         public ObservableCollection<MessageChatDTO> Messages { get; set; }
 
         public MessageModelView()
         {
             Debug.WriteLine("MessageModelView created");
             messageModel = MessageModel.Instance;
+            userModelView = new UserModelView(); 
             Messages = new ObservableCollection<MessageChatDTO>();
         }
 
-        public Dictionary<string, Message> getMessagesByChatId(int chatId)
+
+        public void LoadMessages(int chatId)
         {
             List<Message> messages = messageModel.GetMessagesByChatId(chatId);
             Debug.WriteLine(messages.Count + "Messages loaded");
-            Dictionary<Message, string> messagesDict = new Dictionary<string, Message>();
+            Messages.Clear();
+            messages.Sort((x,y) =>  x.sentDateTime.CompareTo(y.sentDateTime));
             foreach (Message message in messages)
             {
-                messagesDict.Add(message.Id.ToString(), message);
-                Debug.WriteLine("Message loaded: " + message.Content);
+                User user = userModelView.GetUser(message.UserId);
+                Messages.Add(new MessageChatDTO(message.Id, message.Content, message.UserId, message.ChatId, message.sentDateTime, user.Name));
             }
-            return messagesDict;
+
         }
 
 
-        public void setUserIdAndChat(int userId, int chatId)
-        {
-            userId = userId;
-            chatId = chatId;
-        }
 
-        public void loadMessages()
-        {
-            Debug.WriteLine("Loading messages");
-            try
-            {
-                List<Message> messages = messageModel.GetMessagesByChatId(chatId);
-
-                foreach (Message message in messages)
-                {
-                    messagesDict.Add(message.id.ToString(), message);
-                    Debug.WriteLine("Message loaded: " + message.content);
-                }
-                Debug.WriteLine(messages.Count);
-                foreach (KeyValuePair<string, Message> message in messages)
-                {
-                    Messages.Add(message.Value);
-                    Debug.WriteLine("Message loaded: " + message.Value.content);
-                }
-                Debug.WriteLine(Messages.Count + " messages loaded");
-            }
-            catch (Exception e)
-            {
-                throw new Exception("Error while loading messages: " + e.Message);
-            }
-        }
-
-
-        public void BackButtonHandler()
-        {
-            Debug.WriteLine("Back button clicked");
-        }
-
-        public void sendButtonHandler(string msg)
+        public void SendButtonHandler(int userId,int chatId, string msg)
         {
             Debug.WriteLine("Send button clicked");
-            string message = msg;
             DateTime currentDateTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, Config.ROMANIA_TIMEZONE);
-            Message newMessage = new Message(message, userId, chatId, currentDateTime);
-            MessageChatDTO messageChatDTO = new MessageChatDTO(newMessage.Id ,message, userId, chatId, currentDateTime, userModelView.GetUser(userId).ToString());
+            //Message newMessage = new Message(message, userId, chatId, currentDateTime);
+            //MessageChatDTO messageChatDTO = new MessageChatDTO(newMessage.Id ,message, userId, chatId, currentDateTime, userModelView.GetUser(userId).ToString());
+
+            LoadMessages(chatId);
         }
     }
 }
