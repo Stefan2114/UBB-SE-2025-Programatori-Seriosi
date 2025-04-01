e77215f523d7a7384e221307e2950ffaa0f6249c
Paul Schiop <67235267+PaulSchiop@users.noreply.github.com>
2025-04-01 18:38:35 +0300

    Update MessageView.xaml.cs

--- Views/MessageView.xaml.cs
+++ Views/MessageView.xaml.cs
@@ -15,14 +15,8 @@ using Microsoft.UI.Xaml.Navigation;
 using Team3.ModelViews;
 using System.Diagnostics;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Team3.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MessageView : Page
     {
         public MessageModelView ViewModel { get; } = new MessageModelView();
